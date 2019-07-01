f90e31c3fca130c260e1176d16caaf9f61949248
astrarisa <astraris@gmial.com>
2019-07-01 21:20:24 +0100

    lesson-9

--- app/src/main/java/com/astraris/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/astraris/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.astraris.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.astraris.swoosh.Model.Player
@@ -12,6 +13,19 @@ import kotlinx.android.synthetic.main.activity_league.*
 class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
+
+    //This is the instance(состояние значения выбранных кнопок) we're saving after it rotated and destroyed
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+    //after the instance is recreated (onRestoreInstanceState)...
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){   //...we check if instant state is not null...
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER) //...than we go and get Extra_player and set to player
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
